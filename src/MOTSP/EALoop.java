3c0b0a03573d0bed26f91e38b4f26597b9f462ec
olehalvor <olli408@hotmail.com>
2016-05-03 12:37:47 +0200

    1st

--- src/MOTSP/EALoop.java
+++ src/MOTSP/EALoop.java
@@ -4,4 +4,15 @@ package MOTSP;
  * Created by Ole on 03.05.2016.
  */
 public class EALoop {
+    private static int nGenerations = 100;
+    private static int curGen = 0;
+
+
+    public static void Main (String[] args){
+
+        while (curGen < nGenerations ){
+            //TODO: DO LOOP STUFF
+        }
+
+    }
 }
--- src/MOTSP/MOTSP.java
+++ src/MOTSP/MOTSP.java
@@ -4,4 +4,17 @@ package MOTSP;
  * Created by Ole on 03.05.2016.
  */
 public class MOTSP {
+    private int[] genome;
+
+    public MOTSP(){
+        this.genome = generate_random_genome();
+    }
+
+    private int[] generate_random_genome(){
+        //TODO: MAKE DIS
+    }
+
+    public int calc_fitness(){
+        return -1;
+    }
 }
