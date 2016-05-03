8897e8a41deb0b74c055d84020cdc2de683ceb46
olehalvor <olli408@hotmail.com>
2016-05-03 14:31:38 +0200

    Stuff

--- src/MOTSP/EALoop.java
+++ src/MOTSP/EALoop.java
@@ -1,11 +1,18 @@
 package MOTSP;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Ole on 03.05.2016.
@@ -16,16 +23,50 @@ public class EALoop {
 
 
     public static void main (String[] args){
+        ArrayList<ArrayList> test = new ArrayList<ArrayList>();
         try {
-            Workbook wb = WorkbookFactory.create(new File("Distance.xlsx"));
+            test = readFiles("Distance.xlsx");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println(test.get(2).get(0));
+
         System.out.println("hello");
         while (curGen < nGenerations ){
             //TODO: DO LOOP STUFF
             curGen ++;
         }
 
+    }
+
+    private static ArrayList readFiles(String filename) throws Exception{
+        ArrayList<ArrayList<Integer>> outer = new ArrayList<ArrayList<Integer>>();
+        int i = 0;
+        int k;
+        File myFile = new File(filename);
+        FileInputStream fis = new FileInputStream(myFile);
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis); // Return first sheet from the XLSX workbook
+        XSSFSheet mySheet = myWorkBook.getSheetAt(0); // Get iterator to all the rows in current sheet
+        Iterator<Row> rowIterator = mySheet.iterator(); // Traversing over each row of XLSX file
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            ArrayList<Integer> inner = new ArrayList<Integer>();
+            k = 0;
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                if (k != 0) {
+                    inner.add(((int) cell.getNumericCellValue()));
+                }
+                k++;
+            }
+            if ( i != 0) {
+                outer.add(inner);
+            }
+            i++;
+        }
+        return outer;
     }
 }
--- src/MOTSP/MOTSP.java
+++ src/MOTSP/MOTSP.java
@@ -3,8 +3,14 @@ package MOTSP;
 /**
  * Created by Ole on 03.05.2016.
  */
+
 public class MOTSP {
+
+    // http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5
+
     private int[] genome;
+    private int dist_fitness = 0;
+    private int cost_fitness = 0;
 
     public MOTSP(){
         generate_random_genome();
@@ -15,6 +21,6 @@ public class MOTSP {
     }
 
     public int calc_fitness(){
-        return -1;
+        dist_fitness = Math.random();
     }
 }
