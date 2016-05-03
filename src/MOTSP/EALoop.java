608c66af00c4f00aae5242622b4d39094b396657
olehalvor <olli408@hotmail.com>
2016-05-03 13:07:00 +0200

    Stuff

--- src/MOTSP/EALoop.java
+++ src/MOTSP/EALoop.java
@@ -1,5 +1,12 @@
 package MOTSP;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Ole on 03.05.2016.
  */
@@ -9,6 +16,11 @@ public class EALoop {
 
 
     public static void main (String[] args){
+        try {
+            Workbook wb = WorkbookFactory.create(new File("Distance.xlsx"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("hello");
         while (curGen < nGenerations ){
             //TODO: DO LOOP STUFF
