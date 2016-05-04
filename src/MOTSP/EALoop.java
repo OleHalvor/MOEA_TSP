a2afa3eb73298e9cc38906b4f64611081aa618de
olehalvor <olli408@hotmail.com>
2016-05-04 14:06:02 +0200

    Generate random genome and crossover implemented

--- src/MOTSP/EALoop.java
+++ src/MOTSP/EALoop.java
@@ -1,6 +1,6 @@
 package MOTSP;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -14,8 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Random;
+import java.util.Arrays;
 
 /**
  * Created by Ole on 03.05.2016.
@@ -24,31 +23,61 @@ public class EALoop {
     private static int nGenerations = 100, curGen = 0, popSize = 100;
     private static ArrayList<MOTSP> population;
 
-    public static void Main (){
-        initPopulation();
+    public static void main (String[] args){
+        System.out.println("running");
+        MOTSP p1 = new MOTSP();
+        MOTSP p2 = new MOTSP();
+        MOTSP child = crossOver(p1,p2);
+        System.out.println(Arrays.toString(p1.getGenome()));
+        System.out.println(Arrays.toString(p2.getGenome()));
+        System.out.println(Arrays.toString(child.getGenome()));
 
-        while (curGen < nGenerations){
-            makeChildren();
+    }
 
-            adultSelection();
-        }
-
+    public static boolean contains(final int[] arrayS, final int keyS) {
+        String array = Arrays.toString(arrayS);
+        String key = (Integer.toString(keyS));
+        return array.contains(key);
     }
 
     private static MOTSP crossOver(MOTSP p1, MOTSP p2){
         int[] p1_gen = p1.getGenome();
-        Random rand = new Random();
+        int[] p2_gen = p2.getGenome();
+
         int[] genome = new int[48];
+        Arrays.fill(genome, -1);
+
+        Random rand = new Random();
         int cut1 = rand.nextInt(48);
-        int cut2 = rand.nextInt(48-cut1);
+        int cut2 = rand.nextInt(48-cut1)+cut1;
+        System.out.println("Cut 1 "+ cut1 + " Cut 2 "+ cut2);
+        int[] p1_cut = new int[cut2-cut1];
+
+
         for (int i = cut1; i<cut2; i++){
             genome[i] = p1_gen[i];
         }
-        for (int i = 0; i <)
 
+        for (int i = 0; i <48; i++){ //Loop through new genome
+            if (genome[i]==-1){      //Indicates chromosome not set
+                for (int k= 0; k<48; k++){ //Loop over p2 genome to find 1st value not in new genome
+                    int p2_val = p2_gen[k];
+                    boolean found = false;
+                    for (int o=0; o<48; o++){
+                        if (genome[o]==p2_val){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found){
+                        genome[i] = p2_val;
+                        break;
+                    }
+                }
 
-
-        return child;
+            }
+        }
+        return new MOTSP(genome);
     }
 
     private static void initPopulation(){
--- src/MOTSP/MOTSP.java
+++ src/MOTSP/MOTSP.java
@@ -4,6 +4,8 @@ package MOTSP;
  * Created by Ole on 03.05.2016.
  */
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MOTSP {
 
     // http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5
@@ -22,7 +24,22 @@ public class MOTSP {
     }
 
     private void generate_random_genome(){
-        //TODO make
+        int[] new_gene = new int[48];
+        for (int i =0; i<48; i++){
+            new_gene[i] = i;
+        }
+        new_gene = shuffleArray(new_gene);
+        this.genome = new_gene;
+    }
+    private static int[] shuffleArray(int[] ar) {
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+        return ar;
     }
 
     public void setGenome(int[] genome){
@@ -32,6 +49,8 @@ public class MOTSP {
     public int[] getGenome(){
         return this.genome;
     }
+
+
 
     public void calc_fitness(){
         Random rand = new Random();
